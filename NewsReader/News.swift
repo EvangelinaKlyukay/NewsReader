235ac70d39d0684d3b3a6e718dc4019375f4adf0
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2020-08-17 15:18:31 +0300

    News

---
+++ NewsReader/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  NewsReader
+//
+//  Created by Евангелина Клюкай on 17.08.2020.
+//  Copyright © 2020 Евангелина Клюкай. All rights reserved.
+//
+
+import Foundation
+
+class News {
+    
+}
